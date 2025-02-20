c9655a966f7234823c9ed7fd9fef2b2efbe75bee
dsmutin <dvsmutin@gmail.com>
2025-02-20 19:46:12 +0300

    add grid vizualization

--- grid_search.sh
+++ grid_search.sh
@@ -1,7 +1,17 @@
 # Exec:
 # bash grid_search.sh
 
+# Required:
+# R > 4.2 with libs:
+# shiny
+# ggplot2
+# plotly
+# dplyr
+
 #scripts/generator/
 python test_parameters.py \
     -p data/test/general/param_grid_light.json
 
+#R -e 'shiny::runApp("scripts/generator/grid_viz.R")'
+
+Rscript scripts/generator/grid_viz.R data/param_search/param_stats.csv
---
+++ scripts/generator/grid_viz.R
@@ -0,0 +1,22 @@
+library(dplyr)
+library(ggplot2)
+
+pwd <- commandArgs(trailingOnly = TRUE)
+
+df <- read.csv(pwd)
+
+outwd <- paste0(dirname(pwd), "/", "plots")
+dir.create(outwd)
+
+changed <- df %>% 
+  apply(2, function(x) unique(x) %>% length) > 1
+changed[c(2:4, ncol(df))] <- F
+
+
+for(var in names(df)[changed]) {
+  gg <- ggplot(df, aes_string(x = "iteration", y = "mean_hits", color = var, group = var, fill = var)) +
+    geom_point() +
+    geom_smooth(alpha = .3, formula = 'y~x', method = "loess") +
+    theme_minimal()
+  ggsave(paste0(outwd,"/", var, ".svg"), gg, width = 7, height = 4)
+}
