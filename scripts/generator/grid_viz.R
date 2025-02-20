3c8688b7ad6cde99f5189e4c682ff3bee184efbb
dsmutin <dvsmutin@gmail.com>
2025-02-20 19:54:54 +0300

    legend reposition

--- scripts/generator/grid_viz.R
+++ scripts/generator/grid_viz.R
@@ -17,6 +17,8 @@ for(var in names(df)[changed]) {
   gg <- ggplot(df, aes_string(x = "iteration", y = "mean_hits", color = var, group = var, fill = var)) +
     geom_point() +
     geom_smooth(alpha = .3, formula = 'y~x', method = "loess") +
-    theme_minimal()
+    theme_minimal() +
+    theme(legend.position = "bottom")
+    
   ggsave(paste0(outwd,"/", var, ".svg"), gg, width = 7, height = 4)
 }
